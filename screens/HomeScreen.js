63660e389f5efbbe84f79ca47d7256ff268e8d52
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2017-11-07 11:02:55 -0200

    Configured the LogOut event - LogIn and LogOut Fully Functional

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,8 +3,13 @@ import { View } from "react-native";
 import firebase from "firebase";
 import Header from "../components/common/header";
 import LoginForm from "../components/LoginForm";
+import Button from "../components/common/Button";
+import CardSection from "../components/common/CardSection";
+import Spinner from "../components/common/Spinner";
 
 export default class HomeScreen extends Component {
+  state = { loggedIn: false };
+
   static navigationOptions = {
     header: null
   };
@@ -17,12 +22,38 @@ export default class HomeScreen extends Component {
       storageBucket: "auth-3e9bb.appspot.com",
       messagingSenderId: "269691653937"
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large"/>
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Academia" />
-        <LoginForm />
+          {this.renderContent()}
       </View>
     );
   }
